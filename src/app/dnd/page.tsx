ef5acd8e4072e48d5467145a583d6f1f37b821c1
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-12-28 00:23:36 +0900

    ✨feat：add dnd page

---
+++ src/app/dnd/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Contaienr from '@/components/Container'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Contaienr />
+    </div>
+  )
+}
+
+export default page
